9fe20e3853c2e24ae86b32f09a3a0d19c65763e3
tdubuc <theophile.dubuc@ens-lyon.fr>
2020-02-06 12:38:20 +0100

    __str__, __repr__ and __add__ methods

--- PhysUnits.py
+++ PhysUnits.py
@@ -11,27 +11,62 @@ class PhysUnit:
             for u in unit.split('.'):
                 s = u.split('^')
                 if len(s) == 1:
-                    self.unit[s[0]] = 1 #TODO gérer les 'km.km' par ex
+                    try:
+                        self.unit[s[0]] += 1
+                    except KeyError:
+                        self.unit[s[0]] = 1
                 else: 
                     self.unit[s[0]] = int(s[1])
+        
+        else:
+            print("Tu fais n'imp mon pote")
+        #TODO vraie erreur si ce n'est aucun des deux
 
-        #TODO erreur si ce n'est aucun des deux
+    def __str__(self, latex=False):
+        
+        l = []
 
-    def __str__(self):
-        pass #TODO
+        for k in self.unit.keys():
+            s = k
+            if self.unit[k] != 1:
+                s += '^' + str(self.unit[k])
+            l.append(s)
+
+        return str(self.val) + '.'.join(l)
+
 
     def __repr__(self):
-        pass #TODO
+        
+        l = []
+        for k in self.unit.keys():
+            s = k
+            if self.unit[k] != 1:
+                s += '^' + str(self.unit[k])
+            l.append(s)
+
+        return self.__class__.__name__ + '(' + self.val.__repr__() + ',\'' + '.'.join(l) + '\')'
 
 ### Operations on PhysUnit
 
 # Binary operators 
 
     def __add__(self, other):
-        pass #TODO (on ne peut ajouter que des PhysUnits entre eux, et s'ils ont la même unité)
+        
+        if isinstance(other, self.__class__):
+
+            if self.unit != other.unit:
+                print("Inhomogène") #TODO vraie erreur
+                return -1
+            else:
+                return self.__class__(self.val+other.val, dict(self.unit))
+
+        else:
+            print("Erreur de type") #TODO vraie erreur
+            return -1
 
     def __radd__(self, other):
-        pass #TODO
+        
+        return self + other
 
     def __sub__(self, other):
         pass #TODO
